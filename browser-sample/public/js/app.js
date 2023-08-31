a54cef973a992eaddacf88cb459d33f14a844ae2
maxmaci <74024731+maxmaci@users.noreply.github.com>
2023-08-31 17:28:48 +0200

    Update app.js

--- browser-sample/public/js/app.js
+++ browser-sample/public/js/app.js
@@ -1,6 +1,6 @@
 let auth0Client = null;
 
-const fetchAuthConfig = () => fetch("/auth_config.json");
+const fetchAuthConfig = () => fetch("https://gist.githubusercontent.com/maxmaci/7d3730462b5a75c634c42c892e1730b0/raw/edac76e0a0175dc135402c87d32b72a7466d7b7c/auth_config.json");
 
 const configureClient = async () => {
   const response = await fetchAuthConfig();
@@ -23,4 +23,12 @@ const updateUI = async () => {
 
   document.getElementById("btn-logout").disabled = !isAuthenticated;
   document.getElementById("btn-login").disabled = isAuthenticated;
+};
+
+const login = async () => {
+  await auth0Client.loginWithRedirect({
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  });
 };
