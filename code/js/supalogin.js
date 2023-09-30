16dcd477c9f123c4e9035b1c05175bb00dbee770
maxmaci <74024731+maxmaci@users.noreply.github.com>
2023-09-30 17:58:59 +0200

    Update supalogin.js

--- code/js/supalogin.js
+++ code/js/supalogin.js
@@ -1,4 +1,5 @@
 // Capture email and password input fields for sign-up
+let username_signup = document.getElementById("username-sign-up");
 let email_signup = document.getElementById("email-sign-up");
 let password_signup = document.getElementById("password-sign-up");
 
@@ -6,7 +7,24 @@ let doSignUp = async () => {
   const { user, session, error } = await supa.auth.signUp({
     email: email_signup.value,
     password: password_signup.value
-  });
+  })
+  .then((response) => {
+    console.log(response)
+    email_signup.value = "";
+    password_signup.value = "";
+      if (response.error) {
+        showMessage('Email e/o password non validi!');
+      } else {
+        logoutButton.style.display = 'block';
+        closeModal();
+        setTimeout(() => {
+        showMessage("Controlla la casella di posta elettronica per convalidare l'account.");
+        }, 310);
+      }//alert('Logged in as ' + response.data.user.email)
+    })
+  .catch((err) => {
+      showMessage('Email e/o password errata!')
+    });
   //log({ user, session, error });
 };
 
